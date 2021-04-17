532636611818d0ee8701b34b4ef17da46a712bcd
Yeltsin <yeltsinbaquedano@gmail.com>
2021-04-17 02:12:07 -0600

    Se creo archivo para cerrar sesion cerrar_sesion.php

--- html/menu_base.php
+++ html/menu_base.php
@@ -166,7 +166,7 @@
                                 <a class="dropdown-item" href="javascript:void(0)"><i
                                         class="ti-settings me-1 ms-1"></i> Configuracion</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="index.html"><i
+                                <a class="dropdown-item" href="cerrar_sesion.php"><i
                                         class="fa fa-power-off me-1 ms-1"></i> Cerrar Secion</a>
                                 <div class="dropdown-divider"></div>
                                 
