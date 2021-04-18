8bcc98049025ee484aaa85aca8466a466cd62a5b
danielcarmona23 <daniellamarash2000@gmail.com>
2021-04-18 12:30:40 -0600

    Estructura de Email.phpo

--- html/Ecorreo.php
+++ html/Ecorreo.php
@@ -15,8 +15,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'aguileraa18@gmail.com';                     //SMTP username
-    $mail->Password   = 'Alejandro189AAAB';                               //SMTP password
+    $mail->Username   = 'correo';                     //SMTP username
+    $mail->Password   = 'contra';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
